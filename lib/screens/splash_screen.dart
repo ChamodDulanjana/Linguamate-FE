46f47187bbd0a68db4090c8113cec904d65d43d8
chamoddulanjana <chamodperera128@gmail.com>
2026-02-23 20:46:12 +0530

    logo size reduced

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -26,7 +26,7 @@ class _SplashScreenState extends State<SplashScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset('assets/images/logo.png', width: 150, height: 150),
+            Image.asset('assets/images/logo.png', width: 120, height: 120),
             const SizedBox(height: 40),
             Text(
               'LINGUAMATE',
