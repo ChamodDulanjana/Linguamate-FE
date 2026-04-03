c44680bb4126db15174deb5defe6efc4a18ea707
chamoddulanjana <chamodperera128@gmail.com>
2026-04-03 18:03:47 +0530

    implement voice list

---
+++ lib/utils/voices_data.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+final List<Map<String, dynamic>> voiceList = [
+  {
+    "id": "alloy",
+    "name": "Alloy",
+    "description": "Composed and direct",
+    "colors": [const Color(0xFF81D4FA), const Color(0xFF1E88E5)],
+  },
+  {
+    "id": "nova",
+    "name": "Nova",
+    "description": "Energetic and enthusiastic",
+    "colors": [const Color(0xFFCE93D8), const Color(0xFF8E24AA)],
+  },
+  {
+    "id": "verse",
+    "name": "Verse",
+    "description": "Warm and engaging",
+    "colors": [const Color(0xFFFFB74D), const Color(0xFFE64A19)],
+  },
+  {
+    "id": "shimmer",
+    "name": "Shimmer",
+    "description": "Clear and expressive",
+    "colors": [const Color(0xFFB39DDB), const Color(0xFF5E35B1)],
+  },
+  {
+    "id": "echo",
+    "name": "Echo",
+    "description": "Strong and confident",
+    "colors": [const Color(0xFFA5D6A7), const Color(0xFF43A047)],
+  },
+  {
+    "id": "marin",
+    "name": "Marin",
+    "description": "Soft and melodic",
+    "colors": [const Color(0xFF80CBC4), const Color(0xFF00897B)],
+  },
+  {
+    "id": "ash",
+    "name": "Ash",
+    "description": "Deep and grounded",
+    "colors": [const Color(0xFFF2E3B4), const Color(0xFFFFD141)],
+  },
+  {
+    "id": "cedar",
+    "name": "Cedar",
+    "description": "Rich and resonant",
+    "colors": [const Color(0xFFD7CCC8), const Color(0xFF6D4C41)],
+  }
+];
